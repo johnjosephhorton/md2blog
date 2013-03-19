35d37044743644b5165953815262f006d7d7be5e
John Horton <john.joseph.horton@gmail.com>
2013-03-19 11:23:06 -0700

    Add Hard links to assets

--- md2blog.R
+++ md2blog.R
@@ -1,5 +1,9 @@
+#! /usr/bin/Rscript --vanilla --default-packages=utils
+
 library(knitr)
 library(markdown)
+
+resource_dir <- "~/Dropbox/symlinks/md2blog/"
 
 args <- commandArgs(TRUE)
 
@@ -10,15 +14,17 @@ html_name <- paste(base_name, ".html", sep = "")
 
 knit(rmd_name, md_name) # create markdown file 
 
+get.resource <- function(file_name) paste(readLines(paste(resource_dir, file_name, sep="")), collapse = " ")
+
 header <- "<html><head>"
 open.css <- "<style>"
-css <- paste(readLines("markdown.css"), collapse=" ")
+css <- get.resource("markdown.css")
 close.css <- "</style>"
-r_syntax_css <- paste(readLines("r_syntax.css"), collapse = " ")
-r_syntax_js <- paste(readLines("r_syntax.js"), collapse = " ")
+r_syntax_css <- get.resource("r_syntax.css")
+r_syntax_js <- get.resource("r_syntax.js" ) 
 end_header <- "</head><body>"
 fragment <- renderMarkdown(md_name, renderer = 'HTML')
-disqus_code <- paste(readLines("disqus.snippet"), collapse=" ")
+disqus_code <- get.resource("disqus.snippet")
 footer <- "</body></html>"
 
 html <- paste(header, open.css, css, close.css, r_syntax_css, r_syntax_js, end_header, fragment, disqus_code, footer,
