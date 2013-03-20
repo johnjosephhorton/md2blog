df5d6b817edfeadfc720b014756874431ae857e6
John Horton <john.joseph.horton@gmail.com>
2013-03-19 19:06:20 -0700

    Added social sharing and fixed reference issues

--- md2blog.R
+++ md2blog.R
@@ -1,20 +1,22 @@
 #! /usr/bin/Rscript --vanilla --default-packages=utils
-
 library(knitr)
 library(markdown)
 
-resource_dir <- "~/Dropbox/symlinks/md2blog/"
+resource_dir <- "/home/john/Dropbox/symlinks/md2blog/"
 
 args <- commandArgs(TRUE)
-
 rmd_name <- args[[1]]
+
 base_name <- strsplit(rmd_name, '\\.')[[1]][[1]]
 md_name <- paste(base_name, ".md", sep="")
 html_name <- paste(base_name, ".html", sep = "")
 
 knit(rmd_name, md_name) # create markdown file 
 
-get.resource <- function(file_name) paste(readLines(paste(resource_dir, file_name, sep="")), collapse = " ")
+get.resource <- function(file_name) {
+  full_name <- paste(resource_dir, file_name, sep = "")
+  readChar(full_name, file.info(full_name)$size)
+}
 
 header <- "<html><head>"
 open.css <- "<style>"
@@ -22,12 +24,32 @@ css <- get.resource("markdown.css")
 close.css <- "</style>"
 r_syntax_css <- get.resource("r_syntax.css")
 r_syntax_js <- get.resource("r_syntax.js" ) 
+twitter_code <- get.resource("twitter.snippet")
+google_plus_code <- get.resource("google_plus.snippet")
+sharing_code <- get.resource("sharing_buttons.html")
 end_header <- "</head><body>"
-fragment <- renderMarkdown(md_name, renderer = 'HTML')
+
+#fragment <- renderMarkdown(md_name, renderer = 'HTML', renderer.options = getOption('markdown.HTML.options'))
+
+fragment <- markdownToHTML(md_name, options = getOption('markdown.HTML.options'), fragment.only = TRUE)
+
+
 disqus_code <- get.resource("disqus.snippet")
 footer <- "</body></html>"
 
-html <- paste(header, open.css, css, close.css, r_syntax_css, r_syntax_js, end_header, fragment, disqus_code, footer,
+html <- paste(header,
+              open.css,
+              css,
+              close.css,
+              r_syntax_css,
+              r_syntax_js,
+              twitter_code,
+              end_header,
+                fragment,
+              sharing_code, 
+              disqus_code,
+              google_plus_code,
+              footer, 
               sep =" ")
 
 fileConn<-file(html_name)
